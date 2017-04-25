6e899bf47dfc702d41bf03035c96e7b5b2b7978b
Maleika Robinson <mimirobinsononline@gmail.com>
2017-04-24 20:03:31 -0500

    add event into myEvents

--- js/copopup.js
+++ js/copopup.js
@@ -36,13 +36,27 @@ var model = {
 
 
 function render () {
+    //for each event in currentEvents, display in the event section
     model.currentEvents.forEach(function(coEvent) {
         var eventSection = $("<section></section>").attr('class', "event");
         var title = $("<h3></h3>").text("Cowork @ " + coEvent.location_name);
         var when = $("<p></p>").text(coEvent.date + " @ " + coEvent.time.startTime + " - " + coEvent.time.endTime);
-        eventSection.append(title, when);
+        var button = $("<button></button>").text("I'm In!").click(function() {
+            model.myEvents.push(coEvent);
+            render();
+        });
+        eventSection.append(title, when, button);
         $(".upcoming").append(eventSection);
         console.log(eventSection);
+    });
+
+    //for each event in myEvents, display in your-events section
+    model.myEvents.forEach(function(myEvent) {
+        var yourEvent = $("<section><section)").attr('class', 'side-event');
+        var yourTitle = $("<h4></h4>").text("Cowork @" + myEvent.location_name);
+        var yourWhen = $("<p></p>").text(myEvent.date + " @ " + myEvent.time.startTime + " - " + myEvent.time.endTime);
+        yourEvent.append(yourTitle, yourWhen);
+        $(".your-events").append(yourEvent);
     });
 }
 
